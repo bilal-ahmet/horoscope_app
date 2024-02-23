c3d5a967b419409b94b2eb845624d25c1e58c92e
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-02-23 17:00:22 +0300

    horoscope_app

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.pink
+        primarySwatch: Colors.red
       ),
       home: BurcListesi(),
     );
